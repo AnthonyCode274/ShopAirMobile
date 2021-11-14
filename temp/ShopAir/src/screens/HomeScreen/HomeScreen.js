238ff6b637f030f2beeaf35f30222123408f61f1
Hau Nguyen <huuhau.hh47@gmail.com>
2021-11-14 10:23:37 +0700

    Make temp project..

---
+++ temp/ShopAir/src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Text, View } from 'react-native'
+import Styles from './Styles'
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>HomeScreen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
